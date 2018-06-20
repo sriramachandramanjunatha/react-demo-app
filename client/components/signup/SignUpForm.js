bc2369ae1c1d725f9522c03e29c7e6fbff8228b1
sriramachandramanjunatha <sriramaramachandra@gmail.com>
2018-06-20 20:12:48 +0530

    signup form and its state

--- client/components/signup/SignUpForm.js
+++ client/components/signup/SignUpForm.js
@@ -1,31 +1,89 @@
 import React from "react";
+import timezones from "../../data/timezones"
+import map from "lodash/map";
 
 class SignUpForm extends React.Component{
-    
+
     constructor(props){
         super(props);
         this.state={
-            username:''
+            username:'',
+            email:'',
+            password:'',
+            passwordConfirmation:'',
+            timezone:''
         }
+
+
+        //bind context of component rather event
+        this.onChange=this.onChange.bind(this)
+        this.onSubmit=this.onSubmit.bind(this)
+    }
+
+    onChange(e){
+        this.setState({[e.target.name]:e.target.value})
+    }
+
+
+    onSubmit(e){
+        e.preventDefault();
+        console.log(this.state)
     }
     render(){
-        
+
+      const options = map(timezones,(val,key)=><option key={val} value={val}>{key}
+      </option>
+    );
         return (
-            <form>
-            
+            <form onSubmit={this.onSubmit}>
+
                 <h1>
                 Join our community!
                 </h1>
                 <div className="form-group">
-                    
+
                     <label className="control-label">Username</label>
-                    
-                    <input value={this.state.username} type="text" name="username" className="form-control"/>
-                
+
+                    <input value={this.state.username} onChange={this.onChange} type="text" name="username" className="form-control"/>
+
+                </div>
+
+                <div className="form-group">
+
+                    <label className="control-label">Email</label>
+
+                    <input value={this.state.email} onChange={this.onChange} type="text" name="email" className="form-control"/>
+
                 </div>
 
 
+                <div className="form-group">
 
+                    <label className="control-label">Password</label>
+
+                    <input value={this.state.password} onChange={this.onChange} type="password" name="password" className="form-control"/>
+
+                </div>
+
+                <div className="form-group">
+
+                    <label className="control-label">Password Confirmation</label>
+
+                    <input value={this.state.passwordConfirmation} onChange={this.onChange} type="password" name="passwordConfirmation" className="form-control"/>
+
+                </div>
+
+                <div className="form-group">
+                    <label className="control-label">Timezone</label>
+                    <select
+                        className="form-control"
+                        name="timezone"
+                        onChange={this.onChange}
+                        value={this.state.timezone}>
+                        <option value="" disabled>Choose Your Timezone</option>
+                        {options}
+                    </select>
+                    </div>
                 <div className="form-group">
                     <button className="btn btn-primary btn-lg">
                     Sign up </button>
