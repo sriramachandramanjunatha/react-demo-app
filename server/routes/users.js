9f6ed37c5478cd90162a8a42645880b20c99d303
sriramachandramanjunatha <sriramaramachandra@gmail.com>
2018-06-28 17:43:46 +0530

    saved record in psotgres db

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,6 +3,10 @@ import express from 'express';
 // import Validator from 'validator'
 // import isEmpty from 'lodash/isEmpty'
 import validateInput from '../shared/validations/signup'
+
+import bcrypt from 'bcrypt'
+
+import User from '../models/user'
 let router = express.Router()
 
 
@@ -14,7 +18,19 @@ router.post('/',(req, res) => {
   const { errors, isValid } = validateInput(req.body)
 
   if(isValid){
-    res.json({success:true})
+    // res.json({success:true})
+    const { username, password, timezone, email }= req.body
+
+
+    const password_digest = bcrypt.hashSync(password,10);
+
+
+    User.forge({
+      username, timezone, email, password_digest
+    }, {hasTimestamps:true}).save()
+
+    .then(user => res.json({success:true}))
+    .catch(err => res.status(500).json({error:err}))
   }else {
     res.status(400).json(errors);
   }
